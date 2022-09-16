755f48fb7877e06f5b10cd553a0a4672255fdde5
Wolf <wolf.derechter@student.hogent.be>
2022-09-17 00:13:18 +0200

    Refactor options loading, basic css

--- js/canvas.js
+++ js/canvas.js
@@ -1,116 +1,143 @@
-const categories = document.querySelectorAll(".categories > a")
-const options = document.getElementById("optionsUl")
-var currentCategorie;
+const categories = document.querySelectorAll(".categories > a");
+const options = document.getElementById("optionsUl");
+var currentCategory;
 
 class Doll {
-    constructor(){
-        this.options = [];
-    }
+  constructor() {
+    this.options = [];
+    var bodyImg = new Image();
+    bodyImg.src = "/images/body/0.png";
+    this.body = bodyImg;
+  }
 
-    addOption(option){
-        this.options.push(option);
-    }
+  addOption(option) {
+    this.options.push(option);
+  }
 
-    removeOption(option){
-        this.options.forEach((opt, index) => {
-            if(opt.image == option.image && opt.categorie == option.categorie){
-                this.options.splice(index, 1);
-            }
-        })
-    }
+  hasOption(category, image) {
+    var dollHasOption = false;
 
-    removeAllOptionsFromCategorie(categorie){
-        this.options.forEach((opt, index) => {
-            if (index > -1){
-                if(opt.categorie == categorie){
-                    this.options.splice(index, 1);
-                    return;
-                }
-            }
+    this.options.forEach((opt) => {
+      if (opt.category == category && opt.image.src == image.src) {
+        dollHasOption = true;
+      }
+    });
 
-        })
-    }
+    return dollHasOption;
+  }
 
-    draw(){
-        const ctx = canvas.getContext('2d');
-        var x = 100;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        this.options.forEach(opt => {
-            ctx.drawImage(opt.image, x, x, 200, 200)
-            x = x+10;
-        })
-        
-    }
+  removeOption(option) {
+    this.options.forEach((opt, index) => {
+      if (
+        opt.image.src == option.image.src &&
+        opt.category == option.category
+      ) {
+        this.options.splice(index, 1);
+      }
+    });
+  }
+
+  removeAllOptionsFromCategory(category) {
+    this.options.forEach((opt, index) => {
+      if (index > -1) {
+        if (opt.category == category) {
+          this.options.splice(index, 1);
+          return;
+        }
+      }
+    });
+  }
+
+  draw() {
+    const ctx = canvas.getContext("2d");
+    var x = 100;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(this.body, 250, 0, 250, 500);
+
+    this.options.forEach((opt) => {
+      ctx.drawImage(opt.image, 250, 0, 250, 500);
+      // x = x+10;
+    });
+  }
 }
 
 class Option {
-    constructor(categorie, image){
-        this.categorie = categorie;
-        this.image = image;
-    }
+  constructor(category, image) {
+    this.category = category;
+    this.image = image;
+  }
 }
 
 const doll = new Doll();
 
+window.addEventListener("load", () => {
+  const canvas = document.querySelector("#canvas");
 
-window.addEventListener('load', () => {
-    const canvas = document.querySelector('#canvas');
+  // resize the canvas
+  canvas.height = window.innerHeight * 0.75;
+  canvas.width = window.innerWidth * 0.6;
 
-    //resize the canvas
-    canvas.height = window.innerHeight * 0.75;
-    canvas.width = window.innerWidth * 0.60;
-
-    // doll.draw();
+  doll.draw();
 });
 
-//when resizing the browser window, resize the canvas
-window.addEventListener('resize', () => {
-    canvas.height = window.innerHeight * 0.75;
-    canvas.width = window.innerWidth * 0.60;
-})
+// when resizing the browser window, resize the canvas
+window.addEventListener("resize", () => {
+  canvas.height = window.innerHeight * 0.75;
+  canvas.width = window.innerWidth * 0.6;
+});
 
-categories.forEach(cat => {
-    cat.addEventListener('click', (cat) => {
-        //this will empty my options so previous loaded options will be removed
-        options.innerHTML = '';
+categories.forEach((cat) => {
+  cat.addEventListener("click", (cat) => {
+    // this will empty my options so previous loaded options will be removed
+    options.innerHTML = "";
 
-        currentCategorie = cat.target.outerText;
-        var catImageFolder = `./images/${cat.target.innerText}/`;
-        for (let index = 0; index < 10; index++) {
-            var img = new Image()
-            img.src = `./images/${cat.target.innerText}/${index}.jpg`
-            if(img.height != 0){
-                var option = document.createElement('li')
-                var image = document.createElement('img')
-                image.src = `./images/${cat.target.innerText}/${index}.jpg`
-                // option.innerText = `${cat.target.innerText}`
-                option.appendChild(image)
-        
-                option.addEventListener('click', (clickedOption) => {
-                    if(clickedOption.target.parentNode.classList.contains('clickedOption')){
-                        clickedOption.target.parentNode.classList.toggle('clickedOption');
-                        removeOption = new Option(currentCategorie, clickedOption.target)
-                        doll.removeOption(removeOption)
-                        doll.draw();
+    // remove clickedCategory on each item, and add on the last clicked item
+    categories.forEach((category) => {
+      category.classList.remove("clickedCategory");
+    });
+    cat.target.classList.add("clickedCategory");
+    currentCategory = cat.target.innerText;
 
-                    } else {
-                        //new option
-                        clickedOption.target.parentNode.classList.toggle('clickedOption');
+    for (let index = 0; index < 10; index++) {
+      var img = new Image();
+      img.src = `./images/${currentCategory}/${index}.png`;
 
-                        //clickedOption.target should show my image src but is not ideal and should be replaced with something else, when title is clicked => error
-                        newOption = new Option(currentCategorie, clickedOption.target)
+      img.onload = () => {
+        var option = document.createElement("li");
+        var image = document.createElement("img");
+        image.src = `./images/${currentCategory}/${index}.png`;
 
-                        doll.addOption(newOption)
-                        doll.draw()
-                    }
-                    
-                })
-                options.appendChild(option)
-            }
-        }      
-        
-    })
-})
+        if (doll.hasOption(currentCategory, image)) {
+          option.classList.add("clickedOption");
+        }
+        option.appendChild(image);
+
+        option.addEventListener("click", (clickedOption) => {
+          if (
+            clickedOption.target.parentNode.classList.contains("clickedOption")
+          ) {
+            clickedOption.target.parentNode.classList.toggle("clickedOption");
+            removeOption = new Option(currentCategory, clickedOption.target);
+            doll.removeOption(removeOption);
+            doll.draw();
+          } else {
+            // new option
+            clickedOption.target.parentNode.classList.toggle("clickedOption");
+
+            // clickedOption.target should show my image src but is not ideal
+            // and should be replaced with something else, when title is clicked
+            // => error
+            newOption = new Option(currentCategory, clickedOption.target);
+            doll.addOption(newOption);
+
+            doll.draw();
+          }
+        });
+        options.appendChild(option);
+      };
+    }
+  });
+});
 // categories.forEach(cat => {
 //     cat.addEventListener('click', (cat) => {
 //         options.innerHTML = ''
@@ -124,20 +151,20 @@ categories.forEach(cat => {
 //                 image.src = `./images/${cat.target.innerText}/${index}.jpg`
 //                 option.innerText = `${cat.target.innerText}`
 //                 option.appendChild(image)
-        
+
 //                 option.addEventListener('click', (clickedOption) => {
 //                     console.log(clickedOption.target)
 //                     addToCanvas(clickedOption)
 //                 })
 //                 options.appendChild(option)
 //             }
-//         }      
-        
+//         }
+
 //     })
 // })
 
-function addToCanvas(option){
+// function addToCanvas(option){
 
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(option.target, 100, 100, 200, 200)
-}
+//     const ctx = canvas.getContext('2d');
+//     ctx.drawImage(option.target, 100, 100, 200, 200)
+// }
